322fd0e7c8ee7f1b1a316a4aafe1db42fa8f538b
Muhammad Sulaiman <muhammad.sulaiman@emumba.com>
2024-12-13 15:58:34 +0500

    Add second test case

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -5,6 +5,9 @@ clear|npx cypress run --browser chrome --spec "cypress/integration/examples/Test
 clear|npx cypress cache clear|npx cypress install|npx cypress open
 */
 describe("Assignment test suite",function(){
+    beforeEach('Visit saucedemo inventory page',function(){
+        cy.visit('/inventory.html')
+    })
     it("Successful Sign In",function(){
         cy.visit('/index.html')
         cy.get('[data-test="username"]').type('standard_user')
@@ -14,7 +17,16 @@ describe("Assignment test suite",function(){
         cy.get('.inventory_list').find('.inventory_item').should('have.length',6)
     })
     it("Add Items to Cart and Remove Them from the Products Page",function(){
-        
+        cy.get(':nth-child(1) > .pricebar > .btn_primary').should('have.text','ADD TO CART').click()
+        cy.get('.fa-layers-counter').should('be.visible').should('have.text','1')
+        cy.get(':nth-child(2) > .pricebar > .btn_primary').should('have.text','ADD TO CART').click()
+        cy.get('.fa-layers-counter').should('be.visible').should('have.text','2')
+        cy.wait(1000)
+        cy.get(':nth-child(1) > .pricebar > .btn_secondary').should('have.text','REMOVE').click()
+        cy.get(':nth-child(2) > .pricebar > .btn_secondary').should('have.text','REMOVE').click()
+        cy.wait(1000)
+        cy.get(':nth-child(1) > .pricebar > .btn_primary').should('have.text','ADD TO CART')
+        cy.get(':nth-child(2) > .pricebar > .btn_primary').should('have.text','ADD TO CART')
     })
     it("Add Items to Cart and Remove Them from the Checkout Page",function(){
         
