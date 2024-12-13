83148d5407633ccff7102e25a40c58a45532d59c
Muhammad Sulaiman <muhammad.sulaiman@emumba.com>
2024-12-13 15:55:15 +0500

    Add first test case

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -6,7 +6,12 @@ clear|npx cypress cache clear|npx cypress install|npx cypress open
 */
 describe("Assignment test suite",function(){
     it("Successful Sign In",function(){
-        
+        cy.visit('/index.html')
+        cy.get('[data-test="username"]').type('standard_user')
+        cy.get('#password').type('secret_sauce')
+        cy.get('#login-button').click()
+        cy.get('.product_label').should('be.visible').should('have.text','Products')
+        cy.get('.inventory_list').find('.inventory_item').should('have.length',6)
     })
     it("Add Items to Cart and Remove Them from the Products Page",function(){
         
