2843eb212a79ba7a8999b173098cc0cb4d98040a
Muhammad Sulaiman <muhammad.sulaiman@emumba.com>
2024-12-26 14:24:55 +0500

    Add cypress test file

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,9 @@
+///<reference types="cypress"/>
+/*
+clear|npx cypress open
+clear|npx cypress run --browser chrome --spec "cypress/integration/examples/Test1.js"
+clear|npx cypress cache clear|npx cypress install|npx cypress open
+*/
+describe("Assignment test suite",function(){
+    
+})
