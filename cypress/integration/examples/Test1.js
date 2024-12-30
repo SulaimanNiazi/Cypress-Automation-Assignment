36daa467d655f28c336a2951656e77c8604aa459
Muhammad Sulaiman <muhammad.sulaiman@emumba.com>
2024-12-30 10:45:19 +0500

    Integrate cypress-mochawesome-reporter

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -6,7 +6,7 @@ import signInPage from "../../support/pages/signInPage"
 
 /*
 clear|npx cypress open
-clear|npx cypress run --browser chrome --spec "cypress/integration/examples/Test1.js"
+clear|npx cypress run --headed --browser firefox --spec "cypress/integration/examples/Test1.js"
 clear|npx cypress cache clear|npx cypress install|npx cypress open
 */
 
