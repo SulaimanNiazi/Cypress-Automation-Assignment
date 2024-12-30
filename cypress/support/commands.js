52b0217f42ba61815c1be57005750d4f5fe2f70a
Muhammad Sulaiman <muhammad.sulaiman@emumba.com>
2024-12-30 14:05:16 +0500

    Add custom commands getItemButton(selected,number) and sortList(reverse,path)

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -27,3 +27,20 @@ Cypress.Commands.add('sidebarSelect',(num)=>{
     cy.get('.bm-burger-button > button').click()
     cy.get('.bm-menu a').eq(num-1).click()
 })
+Cypress.Commands.add('getItemButton',(selected,number)=>{
+    const path=':nth-child('+number.toString()+') > .pricebar'+(selected?' > .btn_secondary':' > .btn_primary')
+    return cy.get(path)
+})
+Cypress.Commands.add('checkSorted',(path,toFloat,reverse)=>{
+    let list=Array(6)
+    cy.get(path).each(($el,index)=>{
+        let val=$el.text()
+        if(toFloat){
+            val=parseFloat(val.split('$').pop().trim())
+        }
+        list[index]=val
+    }).then(()=>{
+        const sorted=reverse?list.map(x=>x).sort((a,b)=>b-a):list.map(x=>x).sort((a,b)=>a-b)
+        expect(sorted).to.deep.equal(list)
+    })
+})
--- cypress/support/pages/inventoryPage.js
+++ cypress/support/pages/inventoryPage.js
@@ -5,16 +5,16 @@ class inventoryPage extends page{
         cy.get('.inventory_list').find('.inventory_item').should('have.length',6)
     }
     addItem(num){
-        cy.get(':nth-child('+num.toString()+') > .pricebar > .btn_primary').should('have.text','ADD TO CART').click()
+        cy.getItemButton(false,num).should('have.text','ADD TO CART').click()
     }
     verifyCount(num){
         cy.get('.fa-layers-counter').should('be.visible').should('have.text',num.toString())
     }
     verifyNotSelected(num){
-        cy.get(':nth-child('+num.toString()+') > .pricebar > .btn_primary').should('have.text','ADD TO CART')
+        cy.getItemButton(false,num).should('have.text','ADD TO CART')
     }
     removeItem(num){
-        cy.get(':nth-child('+num.toString()+') > .pricebar > .btn_secondary').should('have.text','REMOVE').click()
+        cy.getItemButton(true,num).should('have.text','REMOVE').click()
     }
     visitDetailsPage(num){
         cy.get('.inventory_item_name').eq(num-1).click()
@@ -38,25 +38,10 @@ class inventoryPage extends page{
     }
     verifySorted(reverse){
         cy.get('.product_sort_container option:selected').then((choice)=>{
-            let itemList=Array(6)
             if(choice.text().includes('Price')){
-                cy.get('.pricebar > .inventory_item_price').each(($el,index)=>{
-                    const val=$el.text()
-                    const strnum=val.split('$')
-                    const num=parseFloat(strnum[1].trim())
-                    itemList[index]=num
-                }).then(()=>{
-                    const sorted = reverse?itemList.map(x=>x).sort((a,b)=>b-a):itemList.map(x=>x).sort((a,b)=>a-b)
-                    expect(sorted).to.deep.equal(itemList)
-                })
-            }
-            else{
-                cy.get('.inventory_item_name').each(($el,index)=>{
-                    itemList[index]=$el.text()
-                }).then(()=>{
-                    const sorted=reverse?itemList.map(a=>a).sort().reverse():itemList.map(a=>a).sort()
-                    expect(sorted).to.deep.equal(itemList)
-                })
+                cy.checkSorted('.pricebar > .inventory_item_price',true,reverse)
+            }else{
+                cy.checkSorted('.inventory_item_name',false,reverse)
             }
         })
     }
